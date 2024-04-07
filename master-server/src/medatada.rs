c63c61aed2116990ace17f82b6af95c7cdb11bb2
RustyMaster <dawidmajka@protonmail.com>
2024-04-07 16:39:45 +0200

    Master - Namespace - ls (#15)

--- master-server/src/medatada.rs
+++ master-server/src/medatada.rs
@@ -44,7 +44,7 @@ enum Node {
 impl Node {
     fn mkdir(&mut self, name: &str) -> Result<&mut Node, Error> {
         match self {
-            Node::Directory { name, nodes } => {
+            Node::Directory { nodes, .. } => {
                 let node = nodes.entry(name.to_string()).or_insert(Node::Directory {
                     name: name.to_string(),
                     nodes: HashMap::new(),
@@ -58,6 +58,22 @@ impl Node {
                     ErrorKind::Other,
                     "Path invalid. File not a directory.",
                 ))
+            }
+        }
+    }
+
+    fn ls(&self) -> Vec<&str> {
+        match self {
+            Node::Directory { name, nodes } => nodes
+                .values()
+                .map(|node| match node {
+                    Node::Directory { name, nodes } => name.as_str(),
+                    Node::File { name } => name.as_str(),
+                })
+                .collect(),
+            Node::File { name } => {
+                //TODO: Error
+                Vec::new()
             }
         }
     }
@@ -78,9 +94,11 @@ impl Namespace {
         }
     }
 
-    // TODO: Only from root ? Or should i check if root
+    // Path should always start with root
     pub fn mkdir(&mut self, path: &str) {
         let mut node = &mut self.root;
+        let path = path.strip_prefix('/').unwrap();
+
         for part in path.split('/') {
             // Add validation
             if part.is_empty() {
@@ -90,8 +108,36 @@ impl Namespace {
         }
     }
 
-    pub fn ls(&self, path: &str) -> Vec<String> {
-        vec![]
+    pub fn ls(&self, path: &str) -> Vec<&str> {
+        // TODO: Add separate method to validate path
+
+        let path = path.strip_prefix('/').unwrap();
+
+        let mut node = &self.root;
+        for part in path.split('/') {
+            // Add validation
+            if part.is_empty() {
+                // Error(Invalid dir name)
+            }
+
+            node = match node {
+                Node::Directory { name, nodes } => {
+                    match nodes.get(part) {
+                        Some(node) => node,
+                        None => {
+                            // TODO: Error
+                            todo!()
+                        }
+                    }
+                }
+                Node::File { name } => {
+                    // Error
+                    todo!()
+                }
+            }
+        }
+
+        node.ls()
     }
 
     pub fn create_file(&mut self, path: &str, filename: &str) {}
@@ -104,7 +150,19 @@ mod tests {
     #[test]
     fn mkdir_should_create_dir() {
         let mut namespace = Namespace::new();
-
         namespace.mkdir("/path/to/new/directory");
+
+        let path_dir = namespace.ls("/path");
+        let to_dir = namespace.ls("/path/to");
+        let new_dir = namespace.ls("/path/to/new");
+        let directory_dir = namespace.ls("/path/to/new/directory");
+
+        assert_eq!(path_dir.len(), 1);
+        assert_eq!(path_dir[0], "to");
+        assert_eq!(to_dir.len(), 1);
+        assert_eq!(to_dir[0], "new");
+        assert_eq!(new_dir.len(), 1);
+        assert_eq!(new_dir[0], "directory");
+        assert_eq!(directory_dir.len(), 0);
     }
 }
